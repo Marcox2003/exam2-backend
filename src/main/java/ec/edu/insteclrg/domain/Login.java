0deb5fb3b2be1a9b168387d126287acbfdd63b24
Marco Verdugo <verdugomarco03@gmail.com>
2023-06-15 15:18:11 -0500

    add class login

---
+++ src/main/java/ec/edu/insteclrg/domain/Login.java
@@ -0,0 +1,25 @@
+package ec.edu.insteclrg.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+public class Login {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+	@Column(unique = true, nullable = false)
+	private String username;
+
+	@Column(unique = true, nullable = false)
+	private String password;
+
+}
---
+++ src/main/java/ec/edu/insteclrg/dto/LoginDTO.java
@@ -0,0 +1,17 @@
+package ec.edu.insteclrg.dto;
+
+import ec.edu.insteclrg.domain.Login;
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+	
+	private long id;
+
+	private String username;
+	 
+	private String password;
+
+	
+
+}
